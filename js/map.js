e553523a6ca4fad1df4f9fdb2ff101e831e4fc89
Инна <i.m.makhova@gmail.com>
2021-06-17 15:12:19 +0300

    Поправила вывод типа расмещения, убрала лишнию переменнуж вызова массива данных, очистка данных карточки innerHTML, поправила структуру функции заполнения карточки

--- js/map.js
+++ js/map.js
@@ -6,44 +6,41 @@ const cardTemplate = document.querySelector('#card')
   .content
   .querySelector('.popup');
 
-const advertsArray = getAdvertsArray();
-
-function getAccomodationType(name) {
-  switch (name) {
-    case 'flat':
-      return 'Квартира';
-    case 'bungalow':
-      return 'Бунгало';
-    case 'house':
-      return 'Дом';
-    case 'palace':
-      return 'Дворец';
-    case 'hotel':
-      return 'Отель';
-    default:
-      return 'Непонятно!';
-  }
-}
+const accomodationType = {
+  flat: 'Квартира',
+  bungalow: 'Бунгало',
+  house: 'Дом',
+  palace: 'Дворец',
+  hotel: 'Отель',
+};
 
 function createCard(advert) {
   const card = cardTemplate.cloneNode(true);
   card.querySelector('.popup__title').textContent = advert.offer.title;
   card.querySelector('.popup__text--address').textContent = advert.offer.address;
   card.querySelector('.popup__text--price').textContent = `${String(advert.offer.price)} ₽/ночь`;
-  card.querySelector('.popup__type').textContent = `${getAccomodationType(advert.offer.type)}`;
+  card.querySelector('.popup__type').textContent = accomodationType[advert.offer.type];
   card.querySelector('.popup__text--capacity').textContent = `${String(advert.offer.rooms)} комнаты для ${String(advert.offer.guests)} гостей`;
   card.querySelector('.popup__text--time').textContent = `Заезд после ${advert.offer.checkin}, выезд до ${advert.offer.checkout}`;
 
+  card.querySelector('.popup__avatar').src = advert.author.avatar;
+  if (advert.offer.description.length === 0) {
+    card.querySelector('.popup__description').style.display = 'none';
+  } else {
+    card.querySelector('.popup__description').textContent = advert.offer.description;
+  }
+
   const featuresList = card.querySelector('.popup__features');
+  const photosList = card.querySelector('.popup__photos');
+
+  //очищает список features и photos из шаблона
+  featuresList.innerHTML = '';
+  photosList.innerHTML = '';
+
+  //заполнение features данными
   if (advert.offer.features.length === 0) {
     featuresList.style.display = 'none';
-  }
-  else {
-    //очищает список features
-    featuresList.querySelectorAll('.popup__feature').forEach((feature) => {
-      feature.remove();
-    });
-    //добавляет в список features из данных
+  } else {
     advert.offer.features.forEach((element) => {
       const feature = document.createElement('li');
       feature.classList.add('popup__feature');
@@ -52,23 +49,10 @@ function createCard(advert) {
     });
   }
 
-  if (advert.offer.description.length === 0) {
-    card.querySelector('.popup__description').style.display = 'none';
-  }
-  else {
-    card.querySelector('.popup__description').textContent = advert.offer.description;
-  }
-
-  const photosList = card.querySelector('.popup__photos');
+  //заполнение photos данными
   if (advert.offer.photos.length === 0) {
     photosList.style.display = 'none';
-  }
-  else {
-    //удаляет список фото из шаблона
-    photosList.querySelectorAll('.popup__photo').forEach((photo) => {
-      photo.remove();
-    });
-    //добавляет фото из данных
+  } else {
     advert.offer.photos.forEach((element) => {
       const photo = document.createElement('img');
       photo.classList.add('popup__photo');
@@ -79,13 +63,12 @@ function createCard(advert) {
     });
   }
 
-  card.querySelector('.popup__avatar').src = advert.author.avatar;
   return card;
 }
 
 function createCardsArray() {
-  const cardsArray = new Array(advertsArray.length).fill(null).map((_element, index) => createCard(advertsArray[index]));
+  const cardsArray = new Array(getAdvertsArray().length).fill(null).map((_element, index) => createCard(getAdvertsArray()[index]));
   return cardsArray;
 }
 
-mapCanvas.append(createCardsArray()[2]);
+mapCanvas.append(createCardsArray()[0]);
