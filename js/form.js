dd3009841c657b054927b70294ab7fe8bf418d6b
Инна <i.m.makhova@gmail.com>
2021-06-21 11:56:37 +0300

    Поправила наименование переменной Типы размещения, добавила функции активации и деактивации формы и фильтров карты

--- js/form.js
+++ js/form.js
@@ -1,1 +1,27 @@
-//модуль формы
+const adForm = document.querySelector('.ad-form');
+const mapFilters = document.querySelector('.map__filters');
+
+const formFields = adForm.querySelectorAll('fieldset');
+const mapFilterFields = mapFilters.querySelectorAll('select');
+
+function deactivatePage() {
+  //добавляем стили неактивного состояния
+  adForm.classList.add('ad-form--disabled');
+  mapFilters.classList.add('map__filters--disabled');
+
+  //делаем поля формы и фильтры карты неактивными
+  formFields.forEach((field) => field.setAttribute('disabled', 'disabled'));
+  mapFilterFields.forEach((field) => field.setAttribute('disabled', 'disabled'));
+}
+
+function activatePage() {
+  //удаляем стили неактивного состояния
+  adForm.classList.remove('ad-form--disabled');
+  mapFilters.classList.remove('map__filters--disabled');
+
+  //делаем поля формы и фильтры карты активными
+  formFields.forEach((field) => field.removeAttribute('disabled', 'disabled'));
+  mapFilterFields.forEach((field) => field.removeAttribute('disabled', 'disabled'));
+}
+
+export {deactivatePage, activatePage};
