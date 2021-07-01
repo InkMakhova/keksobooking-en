3401b1d796b4993e8c4f9545cd9685cc218e1b56
Инна <i.m.makhova@gmail.com>
2021-07-01 14:46:33 +0300

    Добавлен файл констант, файл api.js

---
+++ js/constants.js
@@ -0,0 +1,45 @@
+const ADVERTS_NUMBER = 10;
+
+const TYPES = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+  'hotel',
+];
+
+const ACCOMODATION_TYPE = {
+  flat: 'Квартира',
+  bungalow: 'Бунгало',
+  house: 'Дом',
+  palace: 'Дворец',
+  hotel: 'Отель',
+};
+
+const MIN_ACCOMODATION_PRICES = {
+  bungalow: 0,
+  flat: 1000,
+  hotel: 3000,
+  house: 5000,
+  palace: 10000,
+};
+
+const MAX_ROOM_NUMBER = 100;
+const MIN_CAPACITY = 0;
+
+const CHECK_TIMES = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+
+const MAIN_COORDINATES = {
+  lat: 35.68553153747553,
+  lng: 139.75276363268588,
+};
+
+const ACCURACY = 5;
+
+const DEFAULT_AVATAR = 'img/muffin-grey.svg';
+
+export {ADVERTS_NUMBER, TYPES, ACCOMODATION_TYPE, MIN_ACCOMODATION_PRICES, MAX_ROOM_NUMBER, MIN_CAPACITY, CHECK_TIMES, MAIN_COORDINATES, ACCURACY, DEFAULT_AVATAR};
